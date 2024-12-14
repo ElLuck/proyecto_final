566004930414864e04e5607a57ce114a2c949d23
Sebastian <sebaeriz6@gmail.com>
2024-12-14 19:27:30 -0300

    ahora con panel sin funcionar u3ur

--- panel_admin/views.py
+++ panel_admin/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,'panel_admin/index.html')
+
+def widgets(request):
+    return render(request,'panel_admin/widgets.html')
+
+def tables(request):
+    return render(request,'panel_admin/tables.html')
+
+def invoice(request):
+    return render(request,'panel_admin/invoice.html')
+
+def gallery(request):
+    return render(request,'panel_admin/gallery.html')
+
+def buttons(request):
+    return render(request,'panel_admin/buttons.html')
